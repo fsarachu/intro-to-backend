425f69e8ff79d79cd837f444c16b5842cc9525a5
Franco Sarachu <fsarachu@gmail.com>
2016-11-01 23:56:27 -0300

    define valid_month method

--- month_validator.py
+++ month_validator.py
@@ -16,9 +16,18 @@ months = (
 month_abbreviations = {m[:3].lower(): m for m in months}
 
 
+def valid_month(month):
+    month = month.capitalize()
+    if month in months:
+        return month
+    else:
+        return None
+
+
 def main():
-    for k, v in month_abbreviations.items():
-        print k + ': ' + v
+    print valid_month('january')
+    print valid_month('some junk')
+    print valid_month('AUGUST')
 
 
 if __name__ == '__main__':
