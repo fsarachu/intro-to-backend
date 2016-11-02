8c4890472784bb0959a4c6bcc0f9408dd1fe7aba
Franco Sarachu <fsarachu@gmail.com>
2016-11-02 16:37:32 -0300

    Define main() function

--- validators/YearValidator.py
+++ validators/YearValidator.py
@@ -4,3 +4,12 @@ from datetime import datetime
 class YearValidator:
     min_year = 1900
     max_year = datetime.now().year
+
+
+def main():
+    print 'min_year: {}'.format(YearValidator.min_year)
+    print 'max_year: {}'.format(YearValidator.max_year)
+
+
+if __name__ == '__main__':
+    main()
