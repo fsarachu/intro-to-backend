b6eba094639e0e10c667cb67ceb9ed55d8dfd1ef
Franco Sarachu <fsarachu@gmail.com>
2016-11-02 18:36:24 -0300

    Preserve user input after invalid submission

--- main.py
+++ main.py
@@ -6,20 +6,20 @@ form = """
     <form method='post'>
         What is your birthday?
         <br><br>
-        <label>Month: <input type='text' name='month'></label>
-        <label>Day: <input type='text' name='day'></label>
-        <label>Year: <input type='text' name='year'></label>
+        <label>Month: <input type='text' name='month' value='%(month)s'></label>
+        <label>Day: <input type='text' name='day' value='%(day)s'></label>
+        <label>Year: <input type='text' name='year' value='%(year)s'></label>
         <br><br>
         <div style='color: red'>%(error)s</div>
-        <br><br>
+        <br>
         <input type='submit' value='Submit'>
     </form>
 """
 
 
 class MainHandler(webapp2.RequestHandler):
-    def write_form(self, error=''):
-        self.response.write(form % {'error': error})
+    def write_form(self, error='', month='', day='', year=''):
+        self.response.write(form % {'error': error, 'month': month, 'day': day, 'year': year})
 
     def get(self):
         self.write_form()
@@ -29,12 +29,16 @@ class MainHandler(webapp2.RequestHandler):
         from validators.DayValidator import DayValidator
         from validators.YearValidator import YearValidator
 
-        user_month = MonthValidator.validate(self.request.get('month'))
-        user_day = DayValidator.validate(self.request.get('day'))
-        user_year = YearValidator.validate(self.request.get('year'))
+        user_month = self.request.get('month')
+        user_day = self.request.get('day')
+        user_year = self.request.get('year')
 
-        if not (user_month and user_day and user_year):
-            self.write_form('That doesn\'t look good buddy...')
+        valid_month = MonthValidator.validate(self.request.get('month'))
+        valid_day = DayValidator.validate(self.request.get('day'))
+        valid_year = YearValidator.validate(self.request.get('year'))
+
+        if not (valid_month and valid_day and valid_year):
+            self.write_form(error='That doesn\'t look good buddy...', month=user_month, day=user_day, year=user_year)
         else:
             self.response.write('Thanks! That\'s a totally valid date!')
 
