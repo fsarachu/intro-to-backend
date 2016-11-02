2961d942240719ff95bf41634f22a96562dde244
Franco Sarachu <fsarachu@gmail.com>
2016-11-02 17:12:20 -0300

    Define method validate

--- validators/DateValidator.py
+++ validators/DateValidator.py
@@ -10,3 +10,15 @@ class DateValidator:
     month_validator = MonthValidator.MonthValidator.validate
     day_validator = DayValidator.DayValidator.validate
     year_validator = YearValidator.YearValidator.validate
+
+    @staticmethod
+    def validate(month, day, year):
+        valid_month = DateValidator.month_validator(month)
+        valid_day = DateValidator.day_validator(day)
+        valid_year = DateValidator.year_validator(year)
+
+        if valid_month and valid_day and valid_year:
+            valid_date = dict(month=valid_month, day=valid_day, year=valid_year)
+            return valid_date
+        else:
+            return None
