3cc734926791a58dea4bd952f72dd7690fab836a
Franco Sarachu <fsarachu@gmail.com>
2016-11-02 17:17:09 -0300

    Reformat file

--- validators/DateValidator.py
+++ validators/DateValidator.py
@@ -23,11 +23,13 @@ class DateValidator:
         else:
             return None
 
+
 def main():
     print 'validate(\'Jan\', \'mon\', \'1996\'): {}'.format(DateValidator.validate('Jan', 'mon', '1996'))
     print 'validate(\'Jan\', \'mon\', \'2017\'): {}'.format(DateValidator.validate('Jan', 'mon', '2017'))
     print 'validate(\'jasndj\', \'mon\', \'1996\'): {}'.format(DateValidator.validate('jasndj', 'mon', '1996'))
     print 'validate(\'Jan\', \'ladkjs\', \'1996\'): {}'.format(DateValidator.validate('Jan', 'ladkjs', '1996'))
+
 
 if __name__ == '__main__':
     main()
