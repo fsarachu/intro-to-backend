a7cbf85462e20944f186be60bd5a7cec2eb463a8
Franco Sarachu <fsarachu@gmail.com>
2016-11-02 17:42:36 -0300

    Add try except to validate method

--- validators/DayValidator.py
+++ validators/DayValidator.py
@@ -7,14 +7,16 @@ class DayValidator:
 
     @staticmethod
     def validate(day):
-        """Validates if its a number between 1 and 31. Returns the day as an int if valid, else None"""
-        if day and day.isdigit():
-            num_day = int(day)
-            if 1 <= num_day <= 31:
-                return num_day
+        try:
+            if day and day.isdigit():
+                num_day = int(day)
+                if 1 <= num_day <= 31:
+                    return num_day
+                else:
+                    return None
             else:
                 return None
-        else:
+        except:
             return None
 
 
@@ -25,6 +27,7 @@ def main():
 
     print '\n--- Test Validations ---'
     print 'validate(\'23\'): {}'.format(DayValidator.validate('23'))
+    print 'validate(23): {}'.format(DayValidator.validate(23))
     print 'validate(\'34\'): {}'.format(DayValidator.validate('34'))
     print 'validate(\'0\'): {}'.format(DayValidator.validate('0'))
 
