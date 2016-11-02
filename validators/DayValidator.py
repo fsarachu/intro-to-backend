4858ff65933789a59d87d0e6be4a1025c68373f3
Franco Sarachu <fsarachu@gmail.com>
2016-11-02 16:26:54 -0300

    Fix refactor

--- validators/DayValidator.py
+++ validators/DayValidator.py
@@ -19,8 +19,8 @@ class DayValidator:
         """Validates either a 3 letter day abbreviation or a full day. Returns full day if valid, else None"""
         if len(day) == 3:
             short_day = day.lower()
-            if short_day in DayValidator.day_abbreviations:
-                return DayValidator.days[DayValidator.day_abbreviations.index(short_day)]
+            if short_day in DayValidator.day_abbvs:
+                return DayValidator.days[DayValidator.day_abbvs.index(short_day)]
             else:
                 return None
         else:
@@ -32,8 +32,15 @@ class DayValidator:
 
 
 def main():
+    print '\n--- Days ---'
     print DayValidator.days
+
+    print '\n--- Day Abbreviations ---'
     print DayValidator.day_abbvs
+
+    print '\n--- Test Validations ---'
+    print 'validate(\'monday\'): {}'.format(DayValidator.validate('monday'))
+    print 'validate(\'MON\'): {}'.format(DayValidator.validate('MON'))
 
 
 if __name__ == '__main__':
