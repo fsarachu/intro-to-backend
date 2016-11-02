354717e03ea72dffda27b1599df11727bd6d2566
Franco Sarachu <fsarachu@gmail.com>
2016-11-02 16:24:26 -0300

    Define validate method

--- validators/DayValidator.py
+++ validators/DayValidator.py
@@ -14,6 +14,22 @@ class DayValidator:
 
     day_abbvs = tuple([day[:3].lower() for day in days])
 
+    @staticmethod
+    def validate(day):
+        """Validates either a 3 letter day abbreviation or a full day. Returns full day if valid, else None"""
+        if len(day) == 3:
+            short_day = day.lower()
+            if short_day in DayValidator.day_abbreviations:
+                return DayValidator.days[DayValidator.day_abbreviations.index(short_day)]
+            else:
+                return None
+        else:
+            cap_day = day.capitalize()
+            if cap_day in DayValidator.days:
+                return cap_day
+            else:
+                return None
+
 
 def main():
     print DayValidator.days
