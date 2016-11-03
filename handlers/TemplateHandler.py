6ea967c4446d18d07e1a2c7a58e124affaad2c22
Franco Sarachu <fsarachu@gmail.com>
2016-11-03 14:29:33 -0300

    Add food form

--- handlers/TemplateHandler.py
+++ handlers/TemplateHandler.py
@@ -2,4 +2,13 @@ from Handler import Handler
 
 
 class TemplateHandler(Handler):
-    pass
+    form_html = """
+    <form action=''>
+        <h2>Add Food</h2>
+        <input type='text' name='food' autofocus='autofocus'>
+        <input type='submit' value='Add'>
+    </form>
+    """
+
+    def get(self):
+        self.write(self.form_html)
