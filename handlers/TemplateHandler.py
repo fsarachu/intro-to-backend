2d0a998620c324538fefe6618b7b3d189315f596
Franco Sarachu <fsarachu@gmail.com>
2016-11-03 15:16:54 -0300

    Implement output template

--- handlers/TemplateHandler.py
+++ handlers/TemplateHandler.py
@@ -15,8 +15,11 @@ class TemplateHandler(Handler):
     <input type='hidden' name='food' value='%s'>
     """
 
+    item_html = """
+    <li>%s</li>
+    """
+
     shopping_list_html = """
-    <br><br>
     <h2>Shopping List</h2>
     <ul>
     %s
@@ -24,4 +27,21 @@ class TemplateHandler(Handler):
     """
 
     def get(self):
-        self.write(self.form_html)
+        output = self.form_html
+        output_hidden = ""
+        output_items = ""
+
+        items = self.request.get_all('food')
+
+        if items:
+            for item in items:
+                output_hidden += self.hidden_html % item
+                output_items += self.item_html % item.capitalize()
+
+            output_shopping = self.shopping_list_html % output_items
+
+            output += output_shopping
+
+        output = output % output_hidden
+
+        self.write(output)
