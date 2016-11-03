8f9716423077a94edb3a3b4b25bce42769e4524b
Franco Sarachu <fsarachu@gmail.com>
2016-11-03 12:13:28 -0300

    Add MainHandler

---
+++ handlers/MainHandler.py
@@ -0,0 +1,25 @@
+import Handler
+
+
+class MainHandler(Handler):
+    main_page = """
+        <!doctype html>
+        <html lang='en'>
+        <head>
+          <meta charset='UTF-8'>
+          <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+          <meta http-equiv='X-UA-Compatible' content='ie=edge'>
+          <title>Intro to Backend</title>
+        </head>
+        <body>
+          <h1>Intro to Backend</h1>
+          <ul>
+            <li><a href='/form'>Forms</a></li>
+            <li><a href='/template'>Templates</a></li>
+          </ul>
+        </body>
+        </html>
+        """
+
+    def get(self):
+        self.response.write(self.main_page)
