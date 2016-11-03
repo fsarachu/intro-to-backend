b2cd823d4024acc2e94b0724601ad03bfd8ab5ca
Franco Sarachu <fsarachu@gmail.com>
2016-11-03 12:06:05 -0300

    Main page

--- main.py
+++ main.py
@@ -9,8 +9,27 @@ class Handler(webapp2.RequestHandler):
 
 
 class MainHandler(Handler):
+    main_page = """
+        <!doctype html>
+        <html lang='en'>
+        <head>
+          <meta charset='UTF-8'>
+          <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+          <meta http-equiv='X-UA-Compatible' content='ie=edge'>
+          <title>Intro to Backend</title>
+        </head>
+        <body>
+          <h1>Intro to Backend</h1>
+          <ul>
+            <li><a href='/form'>Forms</a></li>
+            <li><a href='/template'>Templates</a></li>
+          </ul>
+        </body>
+        </html>
+        """
+
     def get(self):
-        self.response.write('Hello World!')
+        self.response.write(self.main_page)
 
 
 class FormHandler(Handler):
