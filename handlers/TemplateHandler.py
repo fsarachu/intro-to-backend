c3393f4ae28379ccd19f0cb1fe14a50bb6feaa30
Franco Sarachu <fsarachu@gmail.com>
2016-11-03 14:35:21 -0300

    Add hidden_html and shopping_list_html

--- handlers/TemplateHandler.py
+++ handlers/TemplateHandler.py
@@ -6,8 +6,21 @@ class TemplateHandler(Handler):
     <form action=''>
         <h2>Add Food</h2>
         <input type='text' name='food' autofocus='autofocus'>
+        %s
         <input type='submit' value='Add'>
     </form>
+    """
+
+    hidden_html = """
+    <input type='hidden' name='food' value='%s'>
+    """
+
+    shopping_list_html = """
+    <br><br>
+    <h2>Shopping List</h2>
+    <ul>
+    %s
+    </ul>
     """
 
     def get(self):
