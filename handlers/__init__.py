96021604d098e8330f0f9a8c2bb158d3704acecf
Franco Sarachu <fsarachu@gmail.com>
2016-11-05 01:51:40 -0300

    Link rot13 route to handler

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,3 +2,4 @@ from .FormHandler import FormHandler
 from .MainHandler import MainHandler
 from .TemplateHandler import TemplateHandler
 from .ThanksHandler import ThanksHandler
+from .Rot13Handler import Rot13Handler
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
 import webapp2
+
 from handlers import *
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/form', FormHandler),
     ('/form/thanks', ThanksHandler),
-    ('/template', TemplateHandler)
+    ('/template', TemplateHandler),
+    ('/rot13', Rot13Handler)
 ], debug=True)
