a0a21ec115105ba1edd6dd47e286e636e8dca0b2
Franco Sarachu <fsarachu@gmail.com>
2016-11-05 00:08:54 -0300

    Get rid of commented code

--- handlers/TemplateHandler.py
+++ handlers/TemplateHandler.py
@@ -2,39 +2,6 @@ from Handler import Handler
 
 
 class TemplateHandler(Handler):
-    # hidden_html = """
-    # <input type='hidden' name='food' value='%s'>
-    # """
-    #
-    # item_html = """
-    # <li>%s</li>
-    # """
-    #
-    # shopping_list_html = """
-    # <h2>Shopping List</h2>
-    # <ul>
-    # %s
-    # </ul>
-    # """
-    #
     def get(self):
         items = self.request.get_all('food')
         self.render('shopping_list.html', items=items)
-        #     output = self.form_html
-        #     output_hidden = ""
-        #     output_items = ""
-        #
-        #     items = self.request.get_all('food')
-        #
-        #     if items:
-        #         for item in items:
-        #             output_hidden += self.hidden_html % item
-        #             output_items += self.item_html % item.capitalize()
-        #
-        #         output_shopping = self.shopping_list_html % output_items
-        #
-        #         output += output_shopping
-        #
-        #     output = output % output_hidden
-        #
-        #     self.write(output)
