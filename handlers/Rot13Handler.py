790900be4589d7801282dbfb900d95ee212c2280
Franco Sarachu <fsarachu@gmail.com>
2016-11-05 01:47:47 -0300

    Add Rot13Handler.py and define do_rot13 method

---
+++ handlers/Rot13Handler.py
@@ -0,0 +1,26 @@
+from Handler import Handler
+
+
+class Rot13Handler(Handler):
+    def do_rot13(self, text):
+        upper_a_ascii = 65
+        upper_z_ascii = 90
+        lower_a_ascii = 97
+        lower_z_ascii = 122
+        output = ""
+
+        for c in text:
+            c = ord(c)
+
+            if upper_a_ascii <= c <= upper_z_ascii:
+                c += 13
+                if c > upper_z_ascii:
+                    c += upper_a_ascii - upper_z_ascii - 1
+            elif lower_a_ascii <= c <= lower_z_ascii:
+                c += 13
+                if c > lower_z_ascii:
+                    c += lower_a_ascii - lower_z_ascii - 1
+
+            output += c
+
+        return output
