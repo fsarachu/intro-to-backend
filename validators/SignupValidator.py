e3d9a31fea4096e5a6a6d8485375fe4e79535fe9
Franco Sarachu <fsarachu@gmail.com>
2016-11-07 03:09:16 -0300

    Define valid_username() method

--- validators/SignupValidator.py
+++ validators/SignupValidator.py
@@ -6,5 +6,8 @@ class SignupValidator():
     PASS_RE = re.compile(r'^.{3,20}$')
     EMAIL_RE = re.compile(r'^[\S]+@[\S]+.[\S]+$')
 
+    def valid_username(self, username):
+        return self.USER_RE.match(username)
+
     def valid_password(self, password):
         return self.PASS_RE.match(password)
