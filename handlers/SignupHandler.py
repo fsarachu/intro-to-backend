aadddccc3356293417ed0de1a9d82e97a53469b6
Franco Sarachu <fsarachu@gmail.com>
2016-11-07 03:14:11 -0300

    Define part of post method in SignupHandler.py

--- handlers/SignupHandler.py
+++ handlers/SignupHandler.py
@@ -4,3 +4,12 @@ from Handler import Handler
 class SignupHandler(Handler):
     def get(self):
         self.render('signup.html')
+
+    def post(self):
+        have_error = False
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('verify')
+        email = self.request.get('email')
+
+        self.render('signup.html')
