0075d38198d45aae11990f01069c8e714eb39e28
Franco Sarachu <fsarachu@gmail.com>
2016-11-07 03:08:34 -0300

    Define valid_password() method

--- validators/SignupValidator.py
+++ validators/SignupValidator.py
@@ -5,3 +5,6 @@ class SignupValidator():
     USER_RE = re.compile(r'^[a-zA-Z0-9_-]{3,20}$')
     PASS_RE = re.compile(r'^.{3,20}$')
     EMAIL_RE = re.compile(r'^[\S]+@[\S]+.[\S]+$')
+
+    def valid_password(self, password):
+        return self.PASS_RE.match(password)
