7a997e8513e52fda821437ca24a8f9b5f9b28c64
Franco Sarachu <fsarachu@gmail.com>
2016-11-07 04:09:19 -0300

    Add validation chain to SignupHandler

--- handlers/SignupHandler.py
+++ handlers/SignupHandler.py
@@ -1,7 +1,10 @@
 from Handler import Handler
+from validators import SignupValidator
 
 
 class SignupHandler(Handler):
+    validator = SignupValidator.SignupValidator()
+
     def get(self):
         self.render('signup.html')
 
@@ -13,6 +16,21 @@ class SignupHandler(Handler):
         email = self.request.get('email')
 
         params = dict(username=username, email=email)
+
+        if not self.validator.valid_username(username):
+            params['error_username'] = 'Username is not valid!'
+            have_error = True
+
+        if not self.validator.valid_password(password):
+            params['error_password'] = 'Password is not valid!'
+            have_error = True
+        elif password != verify:
+            params['error_verify'] = 'Passwords do not match!'
+            have_error = True
+
+        if not self.validator.valid_email(email):
+            params['error_email'] = 'Email is not valid!'
+            have_error = True
 
         if have_error:
             self.render('signup.html', **params)
