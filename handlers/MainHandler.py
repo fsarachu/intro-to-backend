5de724b2792e04fd87c355259b3209103f95b90f
Franco Sarachu <fsarachu@gmail.com>
2016-11-10 14:52:38 -0300

    Set and read cookies

--- handlers/MainHandler.py
+++ handlers/MainHandler.py
@@ -3,4 +3,19 @@ from Handler import Handler
 
 class MainHandler(Handler):
     def get(self):
-        self.render('index.html')
+        visits = self.request.cookies.get('visits', '0')
+        if visits.isdigit():
+            visits = int(visits)
+        else:
+            visits = 0
+
+        visits += 1
+
+        self.response.headers.add_header('Set-Cookie', 'visits={}'.format(visits))
+
+        if visits > 80:
+            message = "You are AWESOME! You have visited this page {} times!".format(visits)
+        else:
+            message = "You are not cool enough... You have only visited this page {} times...".format(visits)
+
+        self.render('index.html', message=message)
