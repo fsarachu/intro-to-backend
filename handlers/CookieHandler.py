1a515a40d8040f03695e217c52b8ad59c95395cb
Franco Sarachu <fsarachu@gmail.com>
2016-11-13 16:03:37 -0300

    Secure cookies and change value,hash separator from comma to dash, comma is not allowed

--- handlers/CookieHandler.py
+++ handlers/CookieHandler.py
@@ -5,36 +5,24 @@ from Handler import Handler
 
 class CookieHandler(Handler):
     def get(self):
-        # Cookies handling
-        visits = self.request.cookies.get('visits', self.make_secure_value('0'))
-        visits = self.check_secure_value(visits)
+        user_cookie_str = self.request.cookies.get('visits', self.make_secure_value('0'))
+        user_cookie_val = self.check_secure_value(user_cookie_str)
         
-        if not visits:
-            self.render('cookies.html', message='Ooooops! That cookie doesn\'t looks good')
-            return 
+        if user_cookie_val and user_cookie_val.isdigit():
+            visits = int(user_cookie_val)
+            visits += 1
+            new_cookie_str = self.make_secure_value(str(visits))
+            self.response.headers.add_header('Set-Cookie', 'visits={}'.format(new_cookie_str))
+            self.render('cookies.html', message='You have visited this page {} times'.format(visits))
+        else:
+            self.render('cookies.html', message='Ooooops! That cookie doesn\'t look good...')
         
-        if visits.isdigit():
-            visits = int(visits)
-        else:
-            visits = 0
-
-        visits += 1
-
-        self.response.headers.add_header('Set-Cookie', 'visits={}'.format(visits))
-
-        if visits > 30:
-            message = "You are AWESOME! You have visited this page {} times!".format(visits)
-        else:
-            message = "You are not cool enough... You have only visited this page {} times...".format(visits)
-
-        self.render('cookies.html', message=message)
-
     def hash_str(self, s):
         return hashlib.md5(s).hexdigest()
     
     def make_secure_value(self, s):
-        return '{},{}'.format(s,self.hash_str(s))
+        return '{}-{}'.format(s,self.hash_str(s))
 
     def check_secure_value(self, s):
-        value = (s.split(','))[0]
+        value = s.split('-')[0]
         return value if s == self.make_secure_value(value) else None
