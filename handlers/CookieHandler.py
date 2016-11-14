dceab226763e7acdc5d70378203d1dae37f8d26a
Franco Sarachu <fsarachu@gmail.com>
2016-11-14 16:13:19 -0300

    Reformat files

--- handlers/CookieHandler.py
+++ handlers/CookieHandler.py
@@ -7,7 +7,7 @@ class CookieHandler(Handler):
     def get(self):
         user_cookie_str = self.request.cookies.get('visits', self.make_secure_value('0'))
         user_cookie_val = self.check_secure_value(user_cookie_str)
-        
+
         if user_cookie_val and user_cookie_val.isdigit():
             visits = int(user_cookie_val)
             visits += 1
@@ -16,12 +16,12 @@ class CookieHandler(Handler):
             self.render('cookies.html', message='You have visited this page {} times'.format(visits))
         else:
             self.render('cookies.html', message='Ooooops! That cookie doesn\'t look good...')
-        
+
     def hash_str(self, s):
         return hashlib.md5(s).hexdigest()
-    
+
     def make_secure_value(self, s):
-        return '{}-{}'.format(s,self.hash_str(s))
+        return '{}-{}'.format(s, self.hash_str(s))
 
     def check_secure_value(self, s):
         value = s.split('-')[0]
--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,3 +1,4 @@
+from .CookieHandler import CookieHandler
 from .FormHandler import FormHandler
 from .MainHandler import MainHandler
 from .Rot13Handler import Rot13Handler
@@ -5,4 +6,4 @@ from .SignupHandler import SignupHandler
 from .TemplateHandler import TemplateHandler
 from .ThanksHandler import ThanksHandler
 from .WelcomeHandler import WelcomeHandler
-from .CookieHandler import CookieHandler
+
